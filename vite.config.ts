96329ac6d4f319bf3aa8a9c1dcd5b7870f59773f
vpondtor <virak_pond-tor@brown.edu>
2025-10-26 14:53:45 -0700

    update npm scripts

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import path from 'path'
 
 export default defineConfig({
+    base: "/three/",
     root: path.resolve(__dirname, 'src'),
     server: {
         port: 3000,
