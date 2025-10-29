9e9e50e7bd3cfda10c7db2298842fc3a64a4be01
vpondtor <virak_pond-tor@brown.edu>
2025-10-28 22:55:38 -0700

    flying donut

--- src/index.ts
+++ src/index.ts
@@ -1,31 +1,60 @@
 import * as THREE from 'three'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
+import {Planet} from './planet'
 
+// Constants
+const period = 10; 
+const height = 5;
+
+// Initialize Scene
+const scene = new THREE.Scene();
+scene.background = new THREE.Color( 0xffffff );
+
+const geometry = new THREE.TorusGeometry(0.6);
+const material = new THREE.MeshToonMaterial( {
+    color: 0xe5a369
+});
+const torus = new THREE.Mesh(geometry, material);
+torus.rotation.x = -Math.PI / 2.0;
+scene.add(torus);
+
+const planeGeometry = new THREE.PlaneGeometry(50, 50)
+const planeMaterial = new THREE.MeshToonMaterial();
+const plane = new THREE.Mesh(planeGeometry, planeMaterial);
+plane.position.y = -1;
+plane.rotation.x = -Math.PI / 2.0;
+scene.add(plane);
+
+// Lights
+const light = new THREE.DirectionalLight(0xffffff, 1.0);
+// scene.add(light);
+
+const pointLight = new THREE.PointLight();
+pointLight.position.y = 1.5;
+scene.add(pointLight);
+
+// Setup Renderer
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight)
 document.body.appendChild(renderer.domElement)
 
-const scene = new THREE.Scene();
-const geometry = new THREE.TorusGeometry();
-const material = new THREE.MeshToonMaterial( {
-    color: 0x87ceeb // sky blue
-});
-const torus = new THREE.Mesh(geometry, material);
-scene.add(torus);
-
-const light = new THREE.DirectionalLight(0xffffff, 1.0);
-light.position.set(0, 1, 1)
-scene.add(light);
-
+// Create Camera + Controls
 const camera = new THREE.PerspectiveCamera(100, window.innerWidth / window.innerHeight, 0.1, 100);
+const controls = new OrbitControls(camera, renderer.domElement)
+
 camera.position.set(0, 0, 3);
+controls.update();
 
+let clock = new THREE.Clock();
 function animate() {
-    requestAnimationFrame(animate);
-    torus.rotation.x += 0.05;
-    torus.rotation.y += 0.01;
+    let t = clock.getElapsedTime();
+    torus.position.y = ((t % period) / period) * height;
 
-    // camera.rotateOnAxis(new THREE.Vector3(1, 0, 0), 0.01);
+    requestAnimationFrame(animate);
+    controls.update();
     renderer.render(scene, camera);
+
+    // [-5, 5]
 }
 
 animate();
